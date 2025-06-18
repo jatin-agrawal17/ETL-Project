0c8096b52e729cbb899b473e4524979c8c2fbdb8
jatin-agrawal17 <151388565+jatin-agrawal17@users.noreply.github.com>
2025-06-18 14:36:18 +0530

    data validation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
 from network_security.exception.exception import CustomException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
+
 import sys
 
 if __name__ == "__main__":
@@ -12,6 +14,14 @@ if __name__ == "__main__":
         dataingestion = DataIngestion(dataingestionconfig)
         logging.info("initiate the data ingestion")
         dataingestionartifact = dataingestion.initiate_data_ingestion()
+        logging.info("data initiation completed")
         print(dataingestionartifact)
+        
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise CustomException(e,sys)
--- network_security/constants/training_pipeline/__init__.py
+++ network_security/constants/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH:str = os.path.join("data_schema" , "schema.yaml")
+
 """
     DATA ingestion releted constant
 """
@@ -23,3 +25,12 @@ DATA_INGESTION_DIR_NAME:str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature store"
 DATA_INGESTION_INGESTED_DIR:str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float = 0.2
+
+"""
+    Data validation constant
+"""
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_VALID_DIR:str = "validated"
+DATA_VALIDATION_INVALID_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
